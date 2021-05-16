771f1bc1d1a259dca972aec07196b3312499bba0
Rahul Patel <rpmit1061@gmail.com>
2021-05-16 16:29:27 +0530

    changes in settings

--- making_jods/settings.py
+++ making_jods/settings.py
@@ -100,10 +100,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 AUTH_USER_MODEL = 'accounts.User'
 
-DATABASES = { 'default': { 'ENGINE': 'django.db.backends.sqlite3', 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-	    }}
-
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Database https://docs.djangoproject.com/en/3.2/ref/settings/#databases
